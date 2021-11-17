ca97980959ed3011c683c8661a3973bfcb3c331f
Artem Biryukov <hazard@live.de>
2021-11-17 14:27:29 +0100

    Deletes unnecessary code

--- artem_arbeitsvertrag_cmd.py
+++ artem_arbeitsvertrag_cmd.py
@@ -2,11 +2,9 @@ from os import closerange
 from tkinter import *
 import avlib as la
 import os
-import arbeitsvertrag_gui as ag
 
 mitarbeiterdaten = la.vertragsdaten
 
 la.get_vertrags_input_from_cmd(mitarbeiterdaten)
 la.write_dict_to_configfile(inputdict=mitarbeiterdaten, outputfile="texdata/lat.tex")
-#ag.write_dict_to_configfile_from_gui(inputdict=mitarbeiterdaten, outputfile="texdata/lat.tex")
 os.system("pdflatex texdata/main.tex")
