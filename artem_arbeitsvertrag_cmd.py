4dd33930b05bc72179050bd76528a3bcad067911
Artem Biryukov <hazard@live.de>
2021-11-17 17:19:20 +0100

    adds directory

--- artem_arbeitsvertrag_cmd.py
+++ artem_arbeitsvertrag_cmd.py
@@ -6,3 +6,7 @@ mitarbeiterdaten = la.vertragsdaten
 la.get_vertrags_input_from_cmd(mitarbeiterdaten)
 la.write_dict_to_configfile(inputdict=mitarbeiterdaten, outputfile="texdata/lat.tex")
 os.system("pdflatex texdata/main.tex")
+
+#noch nicht angepasst
+#outputdir = "texdata/"
+#listoutput = os.listdir( outputdir )
